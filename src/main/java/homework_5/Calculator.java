1e276d4e169032ae0ba6db74f2525764b54264eb
Aleksandr <a.galagan@scalablesolutions.io>
2024-03-27 20:41:19 +0000

    homework-3

---
+++ src/main/java/homework_5/Calculator.java
@@ -0,0 +1,13 @@
+package src.main.java.homework_5;
+
+public class Calculator {
+    public static int calculateSquare(int num) {
+        int square = num * num;
+        return square;
+    }
+
+    public static int calculateCube(int num) {
+        int cube = num * num * num;
+        return cube;
+    }
+}
